21260de23f027bdebcd413bd5ad5d202029792f7
alya98 <alya-1998@list.ru>
2018-03-02 17:23:44 +0300

    modified

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-    str = str.split("");
-    var stack = [];
-    for (var i = 0; i < str.length; i++) {
-        if (isOpen(str[i])) stack.push(str[i]);
-        else {
-            if (stack.length === 0) return false;
-            var topOfStack = stack.pop();
-            if (!matches(topOfStack, str[i])) return false;
-        }
+    var arrOfBr=[];
+    for(var i=0;i<bracketsConfig.length;i++){
+        arrOfBr.push(bracketsConfig[i][0] + bracketsConfig[i][1])
     }
-
-    function isOpen(oneOfPar) {
-        for (var j = 0; j < bracketsConfig.length; j++) {
-            if (bracketsConfig[j][0] === oneOfPar) {
-                return true;
-            }
-        }
-        return false;
-    }
-    function matches(top, closedPar) {
-        for (var k = 0; k < bracketsConfig.length; k++) {
-            if (top === bracketsConfig[k][0] && closedPar === bracketsConfig[k][1]) {
-                return true;
-            }
-        }
-        return false;
-    }
-    return stack.length === 0 ? true : false;
+   while(1){
+       if(str=='') return true;
+       var indx=-1;
+       for(var k=0;k<arrOfBr.length;k++){
+           if(str.indexOf(arrOfBr[k])!=-1){
+               indx=k; break;
+           }
+       }
+       if(indx==-1) return false;
+       str=str.replace(arrOfBr[indx],'');
+   }
 }
-//console.log(check('||', [['|', '|']]));
+//console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
